990cb0477c0718ca48e64685d63ff620917cb25b
olegrudwit <olegrudwit@gmail.com>
2022-07-29 15:33:05 +0300

    hello world

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,13 +1,13 @@
 package app;
+
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello Java!");
         System.out.println("Hello Java!");
         System.out.println("Hello Java!");
-        System.out.println("Hello Java!");
-        System.out.println("Hello Java!");
-        System.out.println("Hello Java!");
-        System.out.println("Hello Java!");
-        System.out.println("Hello Java!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
